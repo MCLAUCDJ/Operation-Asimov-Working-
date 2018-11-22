289f2075f121f49d235f46657eec619a9494d18f
MCLAUCDJ <44278056+MCLAUCDJ@users.noreply.github.com>
2018-11-22 23:01:31 +1100

    Fixed Code, Added Dog
    Added dog sprite that Cameron made and changed code from the tutorial game we made to the actual project code. My bad :P

--- js/level.js
+++ js/level.js
@@ -1,171 +1,6 @@
 <<<<<<< HEAD
-var jumpforce = -350
-var gravityforce = 400
-var runspeed = 150
-var jumpmultiplyer = 0
-var levelState = {
-  create: function() {
-    //Some basic controls for the game
-    this.controls = game.input.keyboard.addKeys(
-      {
-        'left': Phaser.KeyCode.A,
-        'right': Phaser.KeyCode.D,
-        'jump' : Phaser.KeyCode.W
-      }
-    );
-    //Add a background
-    game.add.sprite(0,0,'bg');
-
-    platforms = game.add.group();  //A group can help organise sprites within the game
-    platforms.enableBody = true;  //All objects in the group will have physics bodies (hitbox, gravity, velocity etc)
-    var ground = platforms.create(0, game.world.height - 64, 'ground'); //Create a platform called ground at the bottom of the screen
-    ground.scale.setTo(2,2);  //Scale the ground object to fill the bottom of the screen
-    ground.body.immovable = true;  //The ground will not move when another object collides with it
-    var ledge = platforms.create(400, 400, 'ground'); //Create another platform called ledge
-    ledge.body.immovable = true;
-    var ledge = platforms.create(-150, 250, 'ground'); //Create another platform, also called ledge
-    ledge.body.immovable = true;
-
-    baddie = game.add.sprite(90,218,'baddie')
-    game.physics.arcade.enable(baddie);
-    baddie.body.bounce.y = 0.2;
-    baddie.body.gravity.y = gravityforce;
-    baddie.body.collideWorldBounds = true;
-
-    baddie.animations.add('left', [0,1], 10, true)
-    baddie.animations.add('right', [2,3], 10, true)
-
-    player = game.add.sprite(32, game.world.height - 150, 'player');
-    game.physics.arcade.enable(player);
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = gravityforce;
-    player.body.collideWorldBounds = true;
-
-    player.animations.add('left', [0,1,2,3], 10, true);
-    player.animations.add('right', [5,6,7,8], 10, true);
-    player.animations.add('still', [4], 1, false);
-
-
-    //Add any other objects that need to exist
-    stars = game.add.group();
-    game.physics.arcade.enable(stars);
-    stars.enableBody = true;
-    for (var i = 0; i < 12; i++) {
-      newStar = stars.create(i * 70, 0, 'star');
-      newStar.body.gravity.y = 6 + Math.random() * 1.1;
-      newStar.body.bounce.y = 0.7 + Math.random() * 0.5;
-      newStar.body.collideWorldBounds = true;
-    }
-
-    gems = game.add.group();
-    game.physics.arcade.enable(gems);
-    gems.enableBody = true;
-    randX = game.rnd.integerInRange(1, 11);
-    newGem = gems.create((randX * 70 + 32) , 0, 'gem');
-    newGem.body.gravity.y = 6;
-    newGem.body.bounce.y = 0.99;
-
-    //Add any display elements (score, lives etc)
-    scoreText = game.add.text(16, 16, 'Score: ' + game.global.score, { fontSize: '32px', fill: '#ff0000' });
-    livesText = game.add.text(16, 42, 'Lives: ' + game.global.lives, { fontSize: '32px', fill: '#ff0000' });
-
-
-  },
-
-  update: function() {  //This function runs every frame
-    //Collision detection
-    var hitPlatform = game.physics.arcade.collide(player, platforms);
-
-    game.physics.arcade.collide(stars, platforms);
-    game.physics.arcade.collide(gems, platforms);
-    game.physics.arcade.collide(player, platforms);
-    game.physics.arcade.collide(baddie, platforms);
-    game.physics.arcade.overlap(player, platforms);
-    game.physics.arcade.overlap(player, stars, this.CollectStar, null, this);
-    game.physics.arcade.overlap(player, gems, this.CollectGem, null, this);
-    game.physics.arcade.overlap(player, baddie, this.Bump, null, this);
-
-    if (player.body.touching.down && hitPlatform){
-      jumpmultiplyer = 0;
-    }
-
-    //Movement code
-
-    if(player.x < baddie.x){
-      baddie.body.velocity.x = -runspeed - 50;
-      baddie.animations.play('left');
-    }else if (player.x > baddie.x) {
-      baddie.body.velocity.x = runspeed - 50;
-      baddie.animations.play('right')
-    }
-    if(this.controls.left.isDown){
-      player.body.velocity.x = -runspeed;
-      player.animations.play('left');
-    }else if (this.controls.right.isDown) {
-      player.body.velocity.x = runspeed;
-      player.animations.play('right')
-    }else{
-      player.animations.play('still')
-      player.body.velocity.x = 0;
-    }
-    if(this.controls.jump.isDown && player.body.touching.down){
-      player.body.velocity.y = jumpforce;
-    }
-    //Other code
-  },
-
-
-  CollectStar: function(player, star) {
-    game.global.score += jumpmultiplyer * 10 +10;  //Increase the score by 10
-    stars.remove(star);  //Remove the star from the group and the game
-    scoreText.text = 'Score: ' + game.global.score;  //Update the UI display
-    //  play.audio('point')
-    jumpmultiplyer += 1;
-    if(stars.total < 1) { //Has the player collected all the stars?
-      game.state.start('end');
-    }
-  },
-
-  CollectGem: function(player, gem) {
-    game.global.score += jumpmultiplyer * 10 + 50;  //Increase the score by 50
-    gems.remove(gem);
-    scoreText.text = 'Score: ' + game.global.score;  //Update the UI display
-    //  play.audio('point')
-  },
-
-
-  Win: function() {
-    game.state.start('end');  //Go to the win state
-  },
-
-  toggleInvincible: function() {
-    player.invincible = !player.invincible
-  },
-
-  invincibility_tint: function()  {
-    player.tint = 0xff0000
-  },
-
-  invincibility_untint: function()  {
-    player.tint = 0xffffff
-  },
-
-  Bump: function(player, baddie) {
-    if (!player.invincible) { //We only damage the player if not invincible
-      game.global.lives -= 1;      //we start damage flashing and toggle invincibility
-      if (game.global.lives <= 0){
-        game.state.start('end');
-      }
-      this.invincibility_tint();
-      game.time.events.add(250, this.invincibility_untint, this);
-      livesText.text = 'Lives: ' + game.global.lives;
-      this.toggleInvincible();             //and then we add a timer to restore the player to a vulnerable state
-      game.time.events.add(1500, this.toggleInvincible, this);
-    }
-  }
-
-};
-=======
+var baddieNumber = 5;
+var doggyNumber = 5;
 var levelState = {
   create: function() {
 
@@ -193,6 +28,7 @@ var levelState = {
     player.MoveSpeed = 300
     player.hacking = 0
     player.facing = 90;
+    player.invincibleFrames = 250;
 
     playerWeapon = player.addChild(game.make.sprite(0, 0, 'weapon'));
     playerWeapon.anchor.setTo(0.5, 0.5)
@@ -220,15 +56,21 @@ var levelState = {
     playerCharge.bulletKillType = Phaser.Weapon.KILL_DISTANCE;
     playerCharge.bulletKillDistance = 30;
     playerCharge.bulletAngleOffset = 0;
-    playerCharge.bulletSpeed = player.MoveSpeed + 20;
+    playerCharge.bulletSpeed = player.MoveSpeed + 50;
     playerCharge.trackSprite(playerWeapon, 16, 0, true);
 
 
-    enemies = game.add.group(); //A group to hold the enemy objects
-    enemies.enableBody = true;
-    weapons = []; //An array to hold the weapon objects
+    rangedEnemies = game.add.group(); //A group to hold the enemy objects
+    rangedEnemies.enableBody = true;
+    weapon = []; //An array to hold the weapon objects
+
+    meleeEnemies = game.add.group(); //A group to hold the enemy objects
+    meleeEnemies.enableBody = true;
+    melee = []; //An array to hold the weapon objects
 
     this.generateEnemy();
+
+    console.log(game.world.height);
 
   },
 
@@ -242,82 +84,158 @@ var levelState = {
   },
 
   generateEnemy: function(){
-    for(i = 0; i < 5; i++){
+    for(i = 0; i < baddieNumber; i++){
       //Create an enemy in a random position
 
-      baddie = enemies.create(this.game.world.randomX, this.game.world.randomY, 'Baddie');
+      baddie = rangedEnemies.create(this.game.world.randomX, this.game.world.randomY, 'Baddie');
       baddie.anchor.setTo(0.5, 0.5);
       game.physics.arcade.enable(baddie);
       baddie.body.collideWorldBounds = true;
       baddie.hacked = false;
-      baddie.facing = 0
-      baddie.MoveSpeed
+      baddie.facing = Math.floor((Math.random() * 360) + 1);
+      baddie.MoveSpeed = 250;
+      baddie.body.bounce.setTo(1,1);
 
-      //Add a weapon to each enemy
-      Rifle = game.add.weapon(5,'bullet');
+    //Add a weapon to each enemy
+      Rifle = game.add.weapon(3,'bullet');
       Rifle.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
       Rifle.bulletSpeed = 200;
-      Rifle.fireRate = 200;
-      Rifle.bulletAngleVariance = 20;
+      Rifle.fireRate = 300;
+      Rifle.bulletAngleVariance = 0;
       Rifle.trackSprite(baddie, 0, 0, true);  //Fire from enemy position
-      weapons[i] = Rifle;  //Add weapon to array
+      weapon[i] = Rifle;  //Add weapon to array
+
     }
-    //enemies.setAll(baddie.tint, 0xff0000);
+    for(i = 0; i < doggyNumber; i++){
+      //Create an enemy in a random position
+      doggy = meleeEnemies.create(this.game.world.randomX, this.game.world.randomY, 'Doggy');
+      doggy.anchor.setTo(0.5, 0.5);
+      game.physics.arcade.enable(doggy);
+      doggy.body.collideWorldBounds = true;
+      doggy.hacked = false;
+      doggy.alive = true;
+      doggy.facing = Math.floor((Math.random() * 8) + 1) * 45;
+      doggy.MoveSpeed = 400;
+      doggy.body.immovable = true;
+
+      //doggy.tint = i * 0x112233;
+
+      Charge = game.add.weapon(3, 'PointyBullet')
+      Charge.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+      Charge.bulletKillType = Phaser.Weapon.KILL_DISTANCE;
+      Charge.bulletKillDistance = 30;
+      Charge.bulletAngleOffset = 0;
+      Charge.bulletSpeed = player.MoveSpeed + 20;
+      Charge.trackSprite(doggy, 16, 0, true);
+      melee[i] = Charge;
+    }
+    //rangedEnemies.setAll(baddie.tint, 0xff0000);
   },
 
   CollisionDetect : function(){
-    game.physics.arcade.collide(player, enemies);
-    game.physics.arcade.collide(enemies);
-    game.physics.arcade.overlap(USB.bullets, enemies, this.Hack, null, this);
-    game.physics.arcade.overlap(playerRifle.bullets, enemies, this.Hack, null, this);
-    game.physics.arcade.overlap(playerCharge.bullets, enemies, this.Hack, null, this);
-    for(i = 0; i < weapons.length; i++){
-      game.physics.arcade.overlap(weapons[i].bullets, player, this.Hit, null, this);
+    game.physics.arcade.collide(player, rangedEnemies);
+    game.physics.arcade.collide(player, meleeEnemies);
+    game.physics.arcade.collide(rangedEnemies);
+    game.physics.arcade.collide(meleeEnemies);
+    game.physics.arcade.overlap(USB.bullets, rangedEnemies, this.rangeHack, null, this);
+    game.physics.arcade.overlap(USB.bullets, meleeEnemies, this.meleeHack, null, this);
+    game.physics.arcade.overlap(playerRifle.bullets, rangedEnemies, this.rangeHack, null, this);
+    game.physics.arcade.overlap(playerRifle.bullets, meleeEnemies, this.meleeHack, null, this);
+    game.physics.arcade.overlap(playerCharge.bullets, rangedEnemies, this.rangeHack, null, this);
+    game.physics.arcade.overlap(playerCharge.bullets, meleeEnemies, this.meleeHack, null, this);
+    for(i = 0; i < weapon.length; i++){
+      game.physics.arcade.overlap(weapon[i].bullets, player, this.Hit, null, this);
+    }
+    for(i = 0; i < melee.length; i++){
+      game.physics.arcade.overlap(melee[i].bullets, player, this.Hit, null, this);
     }
   },
   //Fire each weapon stored in the array at the player
   EnemyController: function() {
-    for(i = 0; i < weapons.length; i++){
-      enemies.forEach(function(baddie){
-        goAngle = game.physics.arcade.angleBetween(player, baddie);
-        //  goAngle = (goAngle * (180/Math.PI)) - 180;
-        baddie.rotation = goAngle;
+    for(i = 0; i < weapon.length; i++){
+      rangedEnemies.forEach(function(baddie){
+        fireAngle = game.physics.arcade.angleBetween(player, baddie);
+        baddie.rotation = fireAngle;
         if(baddie.alive == true){
-          weapons[i].fireAtSprite(player);
+          weapon[i].fireAtSprite(player);
         }
-        //game.time.events.loop(Phaser.Timer.SECOND, this.randomAngle(), this);
-
-        //if(baddie.body.x > (game.world.x - 30)){
-          game.physics.arcade.velocityFromAngle(baddie.facing, 0, baddie.body.velocity);
-        //}else{
-          game.physics.arcade.velocityFromAngle(baddie.facing, 100, baddie.body.velocity);
-      //  }
+        this.wallCheck(baddie);
+        game.physics.arcade.velocityFromAngle(baddie.facing, baddie.MoveSpeed, baddie.body.velocity);
       }, this);
     }
+    for(i = 0; i < melee.length; i++){
+      meleeEnemies.forEach(function(doggy){
+
+        if (typeof timerGateOpen === 'undefined') {
+            var timerGateOpen = false;
+        }
+
+        //var timerGateOpen = false;
+        if (game.rnd.integerInRange(1, 1000) == 1000) {
+          timerGateOpen = !timerGateOpen;
+        }
+
+        if(doggy.body.velocity != 0){
+          melee[i].fire();
+        }
+        if(doggy.alive == true){
+          //stop = setTimeout(this.TimerGate1, 3000);
+
+          if(timerGateOpen == true){
+                doggy.angle = (Math.round(((game.physics.arcade.angleBetween(player, doggy)) * 180/Math.PI) / 45) * 45) - 180;
+                doggy.facing = doggy.angle
+                game.physics.arcade.velocityFromAngle(doggy.facing, doggy.MoveSpeed, doggy.body.velocity);
+              }
+          }else{
+            clearInterval(stop);
+          }
+      }, this);
+     }
   },
+
+  TimerGate : function(){
+      timerGateOpen = true;
+    },
 
   PlayerMovement : function(){
     if(this.controls.Up.isDown && this.controls.Left.isDown){
-      game.physics.arcade.velocityFromAngle(-135, player.MoveSpeed, player.body.velocity);
+      player.going = -135;
+      game.physics.arcade.velocityFromAngle(player.going, player.MoveSpeed, player.body.velocity);
     }else if(this.controls.Up.isDown && this.controls.Right.isDown){
-      game.physics.arcade.velocityFromAngle(-45, player.MoveSpeed, player.body.velocity);
+      player.going = -45;
+      game.physics.arcade.velocityFromAngle(player.going, player.MoveSpeed, player.body.velocity);
     }else if(this.controls.Down.isDown && this.controls.Right.isDown){
-      game.physics.arcade.velocityFromAngle(45, player.MoveSpeed, player.body.velocity);
+      player.going = 45;
+      game.physics.arcade.velocityFromAngle(player.going, player.MoveSpeed, player.body.velocity);
     }else if(this.controls.Down.isDown && this.controls.Left.isDown){
-      game.physics.arcade.velocityFromAngle(135, player.MoveSpeed, player.body.velocity);
+      player.going = 135;
+      game.physics.arcade.velocityFromAngle(player.going, player.MoveSpeed, player.body.velocity);
     }else if(this.controls.Up.isDown){
-      game.physics.arcade.velocityFromAngle(-90, player.MoveSpeed, player.body.velocity);
+      player.going = -90;
+      game.physics.arcade.velocityFromAngle(player.going, player.MoveSpeed, player.body.velocity);
     }else if(this.controls.Down.isDown){
-      game.physics.arcade.velocityFromAngle(90, player.MoveSpeed, player.body.velocity);
+      player.going = 90;
+      game.physics.arcade.velocityFromAngle(player.going, player.MoveSpeed, player.body.velocity);
     }else if(this.controls.Right.isDown){
-      game.physics.arcade.velocityFromAngle(0, player.MoveSpeed, player.body.velocity);
+      player.going = 0;
+      game.physics.arcade.velocityFromAngle(player.going, player.MoveSpeed, player.body.velocity);
     }else if(this.controls.Left.isDown){
-      game.physics.arcade.velocityFromAngle(180, player.MoveSpeed, player.body.velocity);
+      player.going = 180;
+      game.physics.arcade.velocityFromAngle(player.going, player.MoveSpeed, player.body.velocity);
     }else{
       player.body.velocity.y = 0;
       player.body.velocity.x = 0;
     }
-    this.aim();
+    if(weaponType == playerCharge){
+      playerWeapon.angle = player.going;
+      playerCharge.fire();
+      if(player.body.velocity.x < 0 || player.body.velocity.x > 0 ||player.body.velocity.y < 0 ||player.body.velocity.y > 0){
+        this.toggleInvincible();
+      }
+    }else{
+      this.aim();
+    }
+
 
   },
 
@@ -341,7 +259,7 @@ var levelState = {
     }
     playerWeapon.angle = player.facing;
     if (this.controls.Use.isDown){
-      console.log("bang");
+      //console.log("bang");
       (weaponType).fire();
     }
 
@@ -350,24 +268,71 @@ var levelState = {
 
 
   Hit: function(player, bullet){
-    console.log("ouch");
+    //console.log("ouch");
     bullet.kill();
+    if (!player.invincible) { //We only damage the player if not invincible
+      player.tint = 0xff0000;
+      playerWeapon.tint = 0xff0000;
+      /*game.global.lives -= 1;      //we start damage flashing and toggle invincibility
+      livesText.text = 'Lives: ' + game.global.lives;
+      if (game.global.lives <= 0){
+        game.state.start('end');
+      }*/
+      this.invincibility_tint();
+      this.toggleInvincible();
+      game.time.events.add(player.invincibleFrames, this.invincibility_untint, this);
+      game.time.events.add(player.invincibleFrames, this.toggleInvincible, this);//and then we add a timer to restore the player to a vulnerable state
+    }
+  },
+  toggleInvincible: function() {
+    player.invincible = !player.invincible;
   },
 
-  Hack: function(bullet, enemy){
+  invincibility_tint: function()  {
+    player.tint = 0xff0000;
+    playerWeapon.tint = 0xff0000;
+  },
+
+  invincibility_untint: function()  {
+    player.tint = 0xffffff;
+    playerWeapon.tint = 0xffffff;
+  },
+
+  rangeHack: function(bullet, enemy){
     console.log("hit");
     bullet.kill();
       if(weaponType == USB){
         player.body.x = enemy.body.x;
         player.body.y = enemy.body.y;
+        weaponType = playerRifle;
       }
-    horse = enemies.getIndex(enemy);
+    enemyid = rangedEnemies.getIndex(enemy);
     enemy.kill();
     enemy.alive = false;
-    weapons.splice(horse, 1);
-    weaponType = playerRifle;
+    weapon.splice(enemyid, 1);
+  },
 
+  meleeHack: function(bullet, enemy){
+    console.log("hit");
+    bullet.kill();
+      if(weaponType == USB){
+        player.body.x = enemy.body.x;
+        player.body.y = enemy.body.y;
+        weaponType = playerCharge;
+        player.MoveSpeed = 700;
+      }
+    enemyid = meleeEnemies.getIndex(enemy);
+    enemy.kill();
+    enemy.alive = false;
+    melee.splice(enemyid, 1);
+  },
+
+  wallCheck: function(enemy){
+    if(enemy.body.x >= (game.world.width - enemy.width) || enemy.body.x <= 0 || enemy.body.y >= (game.world.height - enemy.width) || enemy.body.y <= 0){
+      enemy.facing = Math.floor((Math.random() * 360) + 1);
+    }
   }
 
 };
+
 >>>>>>> dc7ba191ca234d217974246454c8d76611988b78
--- js/load.js
+++ js/load.js
@@ -1,23 +1,24 @@
 var loadState = {
   preload: function() {
     //Add a line of text to the screen
-    loadingLabel = game.add.text(80, 150, 'Loading...', {font: '30px Courier', fill: '#ffffff'});
+    loadingLabel = game.add.text(80, 150, 'loading...', {font: '30px Courier', fill: '#ffffff'});
 
     //Load all the assets
     //(the size and number of assets will slow this process down)
     game.load.image('bg', 'assets/background.png');
-    game.load.image('healthpack', 'assets/firstaid.png')
-    game.load.image('gem', 'assets/diamond.png')
-    game.load.image('star', 'assets/star.png')
-    game.load.image('button', 'assets/button.png')
-    game.load.image('ground', 'assets/platform.png')
-    game.load.spritesheet('player', 'assets/Good_Guy_v2.png', 64, 64)
-    game.load.spritesheet('baddie', 'assets/Robot.png', 44, 46)
-    game.load.audio('point','assets/point.mp3')
+	  game.load.image('USB', 'assets/missile.png');
+	  game.load.image('bullet', 'assets/missile_Mk1.png');
+	  game.load.image('weapon', 'assets/Weapon.png');
+    game.load.image('playerBullet', 'assets/Lazer.png');
+    game.load.image('playerPointyBullet', 'assets/Pointy_Blue_Lazer.png');
+    game.load.image('PointyBullet', 'assets/Pointy_Red_Lazer.png');
+    game.load.spritesheet('Player', 'assets/player.png', 32, 48);
+    game.load.spritesheet('Baddie', 'assets/baddie.png', 32, 32);
+    game.load.spritesheet('Doggy', 'assets/Angry_Robot.png', 32, 32);
   },
 
   create: function() {
-    game.state.start('menu'); //Load the menu
+    game.state.start('level'); //Load the menu
   }
 
 };
